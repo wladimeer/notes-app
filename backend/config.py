46c58f2fc80f2d8e03c5d21f111a80b0a977a333
wladimeer <sbenavidescartes@gmail.com>
2025-03-17 20:38:57 -0300

    Origin url added to config file

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,7 @@ load_dotenv(find_dotenv())
 class TestingConfig:
     JWT_KEY = os.getenv("JWT_KEY")
     URL_DATABASE = os.getenv("URL_DATABASE")
+    ORIGIN_URL = os.getenv("ORIGIN_URL")
     SECURE = False
 
 
