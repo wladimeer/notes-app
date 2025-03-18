6a9a569d0766626bb926f21f1bedf44b6eeaf8c0
wladimeer <sbenavidescartes@gmail.com>
2025-03-18 14:27:15 -0300

    Corrections about middlewares on app

--- backend/main.py
+++ backend/main.py
@@ -11,13 +11,14 @@ from schemas.user import UserBase
 import utils.constants as constants
 
 app = FastAPI()
+
 app.add_middleware(SessionMiddleware, secret_key=config.JWT_KEY)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[config.ORIGIN_URL],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
@@ -29,6 +30,9 @@ async def before_request(request: Request, call_next):
         f"{constants.AUTH_PREFIX}{constants.USER_REGISTER_ENDPOINT}",
         constants.ROOT_PATH,
     ]
+
+    if request.method == "OPTIONS":
+        return await call_next(request)
 
     if request.url.path not in skip_verification:
         access_token = request.cookies.get(constants.ACCESS_TOKEN_KEY)
@@ -70,6 +74,32 @@ async def before_request(request: Request, call_next):
                 )
 
         else:
+            if refresh_token:
+                is_valid_refresh_token = verify_token(refresh_token)
+
+                if is_valid_refresh_token:
+                    payload = get_token_data(refresh_token)
+
+                    new_user = UserBase(
+                        id=payload["user_id"],
+                        username=payload["sub"],
+                        password="",
+                    )
+
+                    new_access_token = generate_token(new_user)
+                    response = await call_next(request)
+
+                    response.set_cookie(
+                        key=constants.ACCESS_TOKEN_KEY,
+                        value=new_access_token,
+                        httponly=True,
+                        samesite="Strict",
+                        max_age=60 * 60,
+                        secure=config.SECURE,
+                    )
+
+                    return response
+
             return JSONResponse(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 content={"message": constants.NOT_AUTHORIZED_MESSAGE},
